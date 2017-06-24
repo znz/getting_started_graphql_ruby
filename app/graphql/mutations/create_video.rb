c70a73886a16d8490c3d8e43229242464a08cabb
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2017-06-24 22:40:53 +0900

    rails g graphql:mutation CreateVideo (WIP)

---
+++ app/graphql/mutations/create_video.rb
@@ -0,0 +1,12 @@
+Mutations::CreateVideo = GraphQL::Relay::Mutation.define do
+  name "CreateVideo"
+  # TODO: define return fields
+  # return_field :post, Types::PostType
+
+  # TODO: define arguments
+  # input_field :name, !types.String
+
+  resolve ->(obj, args, ctx) {
+    # TODO: define resolve function
+  }
+end
